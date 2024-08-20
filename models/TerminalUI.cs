8a047e2ae2f6f79fa4cebf61d6d7f47b0f0afdc2
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-08-20 15:40:32 -0400

    add terminal ui

---
+++ models/TerminalUI.cs
@@ -0,0 +1,52 @@
+using System;
+using Terminal.Gui;
+
+public class TerminalUI : Window {
+    public static string? UserName;
+
+    public TerminalUI()
+    {
+        Title = $"Example App ({Application.QuitKey} to quit)";
+
+        // Create input components and labels
+        var usernameLabel = new Label { Text = "Username:" };
+
+        var userNameText = new TextField
+        {
+            // Position text field adjacent to the label
+            X = Pos.Right(usernameLabel) + 1,
+
+            // Fill remaining horizontal space
+            Width = Dim.Fill()
+        };
+
+        var passwordLabel = new Label
+        {
+            Text = "Password:", X = Pos.Left(usernameLabel), Y = Pos.Bottom(usernameLabel) + 1
+        };
+
+        var passwordText = new TextField
+        {
+            Secret = true,
+
+            // align with the text box above
+            X = Pos.Left(userNameText),
+            Y = Pos.Top(passwordLabel),
+            Width = Dim.Fill()
+        };
+
+        // Create login button
+        var btnLogin = new Button
+        {
+            Text = "Login",
+            Y = Pos.Bottom(passwordLabel) + 1,
+
+            // center the login button horizontally
+            X = Pos.Center(),
+            IsDefault = true
+        };
+
+        // Add the views to the Window
+        Add(usernameLabel, userNameText, passwordLabel, passwordText, btnLogin);
+    }
+}
